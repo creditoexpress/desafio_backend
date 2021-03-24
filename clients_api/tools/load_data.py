8ee725a2843bef1db19593302203d8c6694ebb07
MOMachine <marcelomaortiz@hotmail.com>
2021-03-23 23:00:06 -0300

    Primeira API quase pronta, Faltando seeder, iniciando dockers

---
+++ clients_api/tools/load_data.py
@@ -0,0 +1,15 @@
+# from models.clients import Clients
+# import json
+
+
+# def seeders(db, root_dir):
+
+#     print(db)
+
+#     f = open(root_dir + "/resources/clientes_data.json")
+
+#     data = json.load(f)
+#     # print(data[0:100])
+
+#     return True
+

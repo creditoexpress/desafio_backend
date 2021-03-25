9623c93c036aee3619fe0afe60691154ce2ac702
MOMachine <marcelomaortiz@hotmail.com>
2021-03-25 18:30:40 -0300

    remoçao do Dockerfile

--- calculo_api/api/calculos.py
+++ calculo_api/api/calculos.py
@@ -20,25 +20,8 @@ class CalculoApi(Resource):
 
         response = jsonify({
             'message': 'success',
-            'valor_parcela': round(prestacao, 2)
+            'valor_parcelas': round(prestacao, 2)
         })
 
         return response
 
-
-# >>> response = requests.get(request_url, request_body)
-# >>> response
-# <Response [200]>
-# >>> response.content
-# b'{\n  "message": "success", \n  "valor_parcela": 789.93\n}\n'
-# >>> print(response.json())
-# {'message': 'success', 'valor_parcela': 789.93}
-# >>> 
-
-# >>> request_body = {
-# ...     "valor": 10000,
-# ...     "taxa": 0.04,
-# ...     "parcelas": 18
-# ... }
-
-# >> request_url = "http://127.0.0.1:5000/calculo"
--- calculo_api/app.py
+++ calculo_api/app.py
@@ -52,4 +52,4 @@ if __name__ == '__main__':
     # Main entry point when run in stand-alone mode.
     app = get_flask_app()
 
-    app.run(debug=True)
+    app.run(debug=True, host='localhost', port=5004)

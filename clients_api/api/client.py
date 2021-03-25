b8ef2ec70a61d476f1cd2ff4b5e7751d0433349d
MOMachine <marcelomaortiz@hotmail.com>
2021-03-24 21:28:57 -0300

    clients_api finalizada, ficou faltando cuidar das portas

--- clients_api/api/client.py
+++ clients_api/api/client.py
@@ -2,28 +2,33 @@
 from flask import Response, request, jsonify
 from flask_restful import Resource
 
+from bson import json_util
+
 # project resources
 from models.clients import Client
 
 
-# @app.route('/clients/<cpf>&<cel>', methods=['GET'])
-# def get_client(cpf, cel):
-#     client = mongo.db.clients.find_one({'_cpf': cpf, '_cel': cel}) 
-#     response = json_util.dumps(client)
-#     return Response(response, mimetype='application/json')
-
 class ClientsApi(Resource):
     def get(self):
-        print('Aqui')
-        # output = Clients.objects.get()
-        print('Passou')
-        return jsonify({'result': 'output'})
+        clients = Client.objects().fields(id=0).as_pymongo()
+
+        clients_dumps = json_util.dumps(clients)
+
+        return Response(clients_dumps, mimetype='application/json')
 
 class ClientApi(Resource):
     
     def get(self):
         cpf = request.json['cpf']
         cel = request.json['cel']
-        print(cpf)
-        # output = Clients.objects.get(cpf=cpf, celular=cel)
-        return jsonify({'result': 'output'})
+
+        client = Client.objects.get(cpf=cpf, celular=cel)
+
+        client_data = client._data
+
+        client_data.pop('id')
+
+        client_dumps = json_util.dumps(client_data)
+
+        return Response(client_dumps, mimetype='application/json')
+

92bc1de3e00ccb12e8a0056ed3c50a0d1069a719
Matheus <sousa6252@gmail.com>
2021-03-27 22:44:29 -0300

    finished implementation

--- Dockerfile
+++ Dockerfile
@@ -10,16 +10,12 @@ RUN apk add --no-cache python3-dev py3-pip && pip3 install --upgrade pip
 WORKDIR /app
 
 # # Copy everything which is present in my docker directory to working (/app)
-COPY /requirements.txt /app
-COPY clientes.json /app
+COPY . /app
 
 RUN pip3 install -r requirements.txt
 
-COPY ["app.py", "/app"]
-
 # Exposing an internal port
 EXPOSE 5000
-
 
 # Step 4 set default commands
 # These are permanent commands i.e even if user will provide come commands those will be considered as argunemts of this command
--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import json
 from datetime import timedelta
-from flask import Flask, request, json, Response
+import pyprind
+import sys
+from flask import Flask, json
 from flask_mongoengine import MongoEngine
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
-import requests
-
-from utils.taxas import taxas
+from flask_jwt_extended import JWTManager
+from routes.routes import routes
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
     'db': 'db_credito_express',
     # to run locally
-    'host': 'localhost',
-    # 'host': 'mongodb://mongo',
+    # 'host': 'localhost',
+    'host': 'mongodb://mongo',
     'port': 27017
 }
+
 app.config["JWT_SECRET_KEY"] = "credito_express-secret-matheus"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=5)
 jwt = JWTManager(app)
@@ -39,113 +40,26 @@ class User(db.Document):
                 "negativado": self.negativado}
 
 
+@app.before_first_request
 def inicializa_banco():
-    # # DELETE ALL
+    # DELETE ALL
     # User.objects.delete({})
 
     if User.objects().first() is None:
         print("### ADICIONANDO CLIENTES A BASE DE DADOS ###")
         with open('clientes.json') as f:
             file_data = json.load(f)
+        bar = pyprind.ProgBar(int(len(file_data)), bar_char='█', stream=sys.stdout)
         for client in file_data:
+            bar.update()
             User(nome=str(client['nome']), cpf=str(client['cpf']), celular=str(client['celular']),
                  score=int(client['score']), negativado=bool(client['negativado'])).save()
+
     else:
         print("### USANDO BASE DE DADOS EXISTENTE ###")
 
 
-inicializa_banco()
-
-
-@app.route("/login", methods=['POST'])
-def login():
-    data = request.json
-
-    if data is None or data == {}:
-        return Response(response=json.dumps({"message": "Por favor informe os campos"}),
-                        status=400,
-                        mimetype='application/json')
-
-    cpf = str(data['cpf'])
-    celular = str(data['celular'])
-
-    user = User.objects(cpf=cpf, celular=celular).first()
-    access_token = create_access_token(identity=str(user._id))
-
-    return Response(
-        content_type="application/json",
-        response=json.dumps({
-            "user": user.to_json(),
-            "token": access_token
-        }),
-        status=200,
-    )
-
-
-@app.route("/simular", methods=['POST'])
-@jwt_required()
-def simular_emprestimo():
-    data = request.json
-
-    if data is None or data == {}:
-        return Response(response=json.dumps({"message": "Por favor informe os campos"}),
-                        status=400,
-                        mimetype='application/json')
-
-    current_user_id = get_jwt_identity()
-
-    user = User.objects(_id=current_user_id).first()
-
-    valor = data['valor']
-    n_parcelas = data['numeroParcelas']
-
-    # negativado
-    if user.negativado:
-        taxa_juros = taxas[0]['taxas'][str(n_parcelas)]
-        print("TAXA DE JUROS APLICADA: " + str(taxa_juros) + "\nTIPO: " + str(taxas[0]['tipo']))
-    # alto
-    elif user.score > 500:
-        taxa_juros = taxas[1]['taxas'][str(n_parcelas)]
-        print("TAXA DE JUROS APLICADA: " + str(taxa_juros) + "\nTIPO: " + str(taxas[1]['tipo']))
-    # baixo
-    else:
-        taxa_juros = taxas[2]['taxas'][str(n_parcelas)]
-        print("TAXA DE JUROS APLICADA: " + str(taxa_juros) + "\nTIPO: " + str(taxas[2]['tipo']))
-
-    endpoint = "https://us-central1-creditoexpress-dev.cloudfunctions.net/teste-backend"
-
-    body = {
-        "valor": valor,
-        "numeroParcelas": n_parcelas,
-        "taxaJuros": taxa_juros
-    }
-
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    response = requests.post(endpoint, json=body, headers=headers)
-
-    return Response(
-        content_type="application/json",
-        response=json.dumps(response.json()),
-        status=200,
-    )
-
-
-@app.route("/users", methods=['GET'])
-@jwt_required()
-def index():
-    users = [user.to_json() for user in User.objects()]
-
-    print("QUANTIDADE DE CLIENTES: " + str(len(users)))
-
-    return Response(
-        content_type="application/json",
-        response=json.dumps({"users": users}),
-        status=200,
-    )
-
+app.register_blueprint(routes)
 
 if __name__ == "__main__":
     app.run(port=5000, host='0.0.0.0')

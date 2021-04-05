7263599542e356f9210c7b332c95a437a361c80f
Chrystian R. Campos <chrystianr@kyros.com.br>
2021-04-05 12:36:25 -0300

    fix cors and gunicorn

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, Flask
+from flask_cors import CORS
+
 from application.controllers.auth import auth
 from application.controllers.loan import loan
 
+
 application = Flask(__name__)
+CORS(application)
 application.register_blueprint(auth, url_prefix='/api/auth')
 application.register_blueprint(loan, url_prefix='/api/loan')
 
 if __name__ == '__main__':
-    application.run()
+    application.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT', 80)))

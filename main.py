ec6477b8da1394d4eb2e1a88dd81d32527384ad1
Chrystian R. Campos <chrystianr@kyros.com.br>
2021-04-05 19:11:06 -0300

    fixing flask app name

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from application.controllers.auth import auth
 from application.controllers.loan import loan
 
 
-application = Flask(__name__)
-CORS(application)
-application.register_blueprint(auth, url_prefix='/api/auth')
-application.register_blueprint(loan, url_prefix='/api/loan')
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(auth, url_prefix='/api/auth')
+app.register_blueprint(loan, url_prefix='/api/loan')
 
 if __name__ == '__main__':
-    application.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT', 80)))
+    app.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT', 80)))
